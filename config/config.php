65a1ad32e31538d9a0a1800e796f9a461fc8ff22
Michael Gagliano <michael@gagliano.me>
2015-06-29 19:22:29 -0400

    Condensed and abstracted

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 $config = array();
 
-$config["filename"] = "/var/www/.openttd/save/" . $_GET["user"] . "/openttd.cfg";
+$config["filename"] = "/var/www/.openttd/save/" . $_COOKIE["username"] . "/openttd.cfg";
 $config["content_download_data_dir"] = "/var/www/.openttd/content_download/";
 $config["grf_files_dir"] = "/var/www/.openttd/newgrf";
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require utilities.php
 $dbhost = 'localhost';
 $dbuser = 'USERNAME';
 $dbpass = 'PASSWORD';
@@ -9,8 +10,7 @@ if (!$dbconn){
 	die("Connection failed: " . mysqli_connect_error());
 }
 
-$method = $_POST["method"];
-if($method == 'register'){
+if($_POST["method"] == 'register'){
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	$query = "INSERT INTO users VALUES (DEFAULT, '" . $username . "', '" . $password . "')";
@@ -24,6 +24,19 @@ if($method == 'register'){
 		</div> ";
 	};
 };
+
+if($_POST["method"] == 'create'){
+	if($_POST["port"] > 0 & !isset($_POST["pid"])) {
+		$port = $_POST["port"];
+		$saveFile = '/var/www/.openttd/save/' . $_COOKIE["username"] . '/' . $_POST["filename"] .'';
+		$config = '/var/www/.openttd/save/' . $_COOKIE["username"] . '/openttd.cfg';
+	//	create_OTTD($port,$saveFile,$config);
+	};
+};
+
+if($_POST["method"] == 'destroy'){
+	destroy_OTTD($_POST["pid"]);
+}
 ?>
 
 <html>
@@ -43,10 +56,26 @@ if($method == 'register'){
 	<input type='hidden' name='method' value='register'>
 	<input type='submit' value='Register'> </center>
 	</div> "; };
-?>
-
-<?php if(isset($_COOKIE["username"])){
-	echo "<h2>Welcome " . $_COOKIE['username'] . "!</h2>";
+if(isset($_COOKIE["username"])){
+	if($_POST["port"] < 0 & $_POST["method"] == 'create'){ echo("Negative Port!"); };
+		echo '<html><body><h2>Create a Server</h2><form action="index.php" method="post">Filename (use final_version.sav) <input type="text" name="filename"><br>Port: <input type="text" name="port" maxlength="5"><input type="hidden" name="method" value="create"><input type="submit"></form>';
+		echo '<h2>View User Saves</h2><br>';
+		$user = $_COOKIE["username"];
+		$fileDir = '/var/www/.openttd/save/' . $user;
+		if ($handle = opendir($fileDir)) {
+			while (false !== ($entry = readdir($handle))) {
+				if ($entry != "." && $entry != ".." && strpos($entry, '.sav') !==false) {
+					echo "$entry <br>";
+				}
+			}
+		closedir($handle);
+		};
+	echo '<h2>Destroy a Server</h2><form ation="index.php" method="post">Port: <input type="text" name="pid"><br><input type="hidden" name="method" value="destroy"><input type="submit"></body></html>';
+	echo '<h2>Running Servers</h2>';
+	exec("ps ax|grep openttd|grep -v grep", $output);
+	foreach ($output as $entry) {
+		echo $entry;
+	};
 };
 ?>
 </body>
